46ea2f568e48fce050e62d7384dcb1da1fdf28b1
Serhii-Luhansky <luganskiy1983@gmail.com>
2023-09-05 11:32:06 +0300

    виправлене №1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,19 @@
+// const itemElements = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${itemElements.length}`);
+// const allElements = itemElements.forEach(element => {
+//     console.log(`Category: ${element.firstElementChild.textContent}`)
+//     console.log(`Elements: ${element.querySelectorAll("li").length}`)
+// })
+// const categoriesList = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${categoriesList.length}`)
+
+// categoriesList.forEach(item => {
+//     console.log(`Category: ${item.firstElementChild.textContent}`);
+//     console.log(`Elements: ${item.lastElementChild.children.length}`)
+// });
 const itemElements = document.querySelectorAll(".item");
 console.log(`Number of categories: ${itemElements.length}`);
 const allElements = itemElements.forEach(element => {
     console.log(`Category: ${element.firstElementChild.textContent}`)
-    console.log(`Elements: ${element.querySelectorAll("li").length}`)
+    console.dir(`Elements: ${element.lastElementChild.children.length}`)
 })
